f52a8b6e224bac6527ec53ee5b804ff6418a82cd
Rahmat Sulistio <rr7465457@gmail.com>
2020-08-12 12:08:21 +0700

    :sparkles: add get province and city api

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require("express");
+const axios = require("axios").default;
+
 const app = express();
 const port = process.env.PORT || 3000;
+const apiKey = "1e096071642f346a1881b4733ebc4cff";
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+// Get All Provinces
+app.get("/province", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://api.rajaongkir.com/starter/province",
+      {
+        headers: { key: apiKey },
+      }
+    );
+    res.json(response.data.rajaongkir);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Get All Cities by ProvinceID
+app.get("/city", async (req, res) => {
+  const provinceId = req.query.province_id;
+
+  try {
+    const response = await axios.get(
+      "https://api.rajaongkir.com/starter/city",
+      {
+        headers: { key: apiKey },
+        params: {
+          province: provinceId,
+        },
+      }
+    );
+    res.json(response.data.rajaongkir);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => console.log(`Running server on port ${port}`));
